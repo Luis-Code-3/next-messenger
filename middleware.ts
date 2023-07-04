318b19f397c5b164bb2086d072f008138cd3223a
Luis Cordova <ageofculturebiz@gmail.com>
2023-07-04 17:33:29 -0400

    add rate limiter for api routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,14 @@
 import {withAuth} from "next-auth/middleware";
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+
+const rateLimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(10, '10 s'),
+    prefix: "@upstash/ratelimit"
+});
 
 export default withAuth(
     async function middleware(req: NextRequest) {
@@ -8,18 +16,28 @@ export default withAuth(
         const isAuth = await getToken({req});
 
         if (pathname.startsWith('/api')) {
+            let success;
+            
+            if(isAuth) {
+                const identifier = isAuth.username;
+                ({ success } = await rateLimit.limit(identifier));
+            } else {
+                const identifier = req.ip ?? '127.0.0.1';
+                ({ success } = await rateLimit.limit(identifier));
+            }
+
+            if(!success) return NextResponse.json({message: "Too many requests. Try again later"}, {status: 429})
+
             if(pathname === '/api/users/register') {
                 if(isAuth) {
-                    return NextResponse.json({message: "You are already registered."}, {status: 400})
+                    return NextResponse.json({message: "You are already registered."}, {status: 409})
                 }
                 return NextResponse.next();
             }
 
             if(!isAuth) {
-                return NextResponse.json({message: "Must be Logged In"}, {status: 400})
+                return NextResponse.json({message: "Must be Logged In"}, {status: 401})
             }
-
-            // add rate limiter here later
 
             return NextResponse.next();
         }
